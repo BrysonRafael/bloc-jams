1eb9fe9a9aea8a08c6d8a7e9edd770fa048c81ea
Bryson Rafael <bryson.rafael@gmail.com>
2016-08-05 14:53:56 -0500

    Animate points with jQuery fixed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,12 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
     
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    $.each($('.point'), revealPoint);
 };
 
 $(window).load(function() {
@@ -18,8 +14,8 @@ $(window).load(function() {
         animatePoints();
     }
     
-    var scrollDistance = $('.sellingPoints').offset().top - $(window).height() + 200;
-    $(window).scroll(function(event){
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    $(window).scroll(function(event) {
         if ($(window).scrollTop() >= scrollDistance) {
             animatePoints();
         }
